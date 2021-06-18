db2e50c501cabe2327e6f918c3a21b8d8f1edd3a
GitHub Action <action@github.com>
2021-06-18 02:15:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/smilingboy/vmbindingtemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.smilingboy.vmbindingtemplate.services
+
+import com.github.smilingboy.vmbindingtemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/smilingboy/vmbindingtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smilingboy/vmbindingtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.smilingboy.vmbindingtemplate.services
 
+import com.github.smilingboy.vmbindingtemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
